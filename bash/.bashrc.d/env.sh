ef8ae0d0eea35515356c2d92dd3da941b2b8c9ec
Albert Z Wang <albert.zhao.wang@gmail.com>
2018-01-02 17:34:49 -0500

    add go to bash

--- bash/.bashrc.d/env.sh
+++ bash/.bashrc.d/env.sh
@@ -2,3 +2,9 @@ export PAGER=less
 export EDITOR=vim
 
 export PATH="${HOME}/.local/bin:${PATH}"
+
+if [[ -d "${HOME}/go" ]]
+then
+    export GOPATH="${HOME}/go"
+    export PATH="${GOPATH}/bin:${PATH}"
+fi
